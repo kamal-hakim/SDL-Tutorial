0009a3f96d96346baf885f3d4e5ffedf301d3b83
Kamal Hakim <kamalhakim.zainal@gmail.com>
2017-10-30 23:20:27 +0800

    Initial Commit

---
+++ SDL-Tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL-Tutorial
+//
+//  Created by Kamal Hakim on 30/10/2017.
+//  Copyright © 2017 Kamal Hakim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
